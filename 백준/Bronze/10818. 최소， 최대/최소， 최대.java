d6f241b188895a8dc232165aaab24f7967ae724f
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-01-13 11:44:26 +0900

    [Bronze III] Title: 최소, 최대, Time: 508 ms, Memory: 88000 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        int max = -1000000;
+        int min = 1000000;
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            if (max < number) max = number;
+            if (min > number) min = number;
+        }
+        System.out.println(min + " " + max);
+    }
+}
