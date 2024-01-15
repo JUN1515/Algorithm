ead3486f73aa91a2e7f80a7efc18c3fb178a8dd8
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-01-15 21:09:33 +0900

    [Gold II] Title: 미확인 도착지, Time: 256 ms, Memory: 41324 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -1,51 +1,53 @@
-from heapq import heappop, heappush
+import sys
+from heapq import *
 
-def deijkstra(start):
-    dist = [INF for _ in range(N+1)]
-    dist[start]=0
-    heap =[]
-    heappush(heap, [0,start])
+def Dijkstra(N, start):
+    global INF, G
+    distance = [INF] * (N + 1)
+    distance[start] = 0
+
+    heap = [(0, start)]
+    heapify(heap)
+
+    # 다익스트라 알고리즘
     while heap:
-        weight,node = heappop(heap)
-        
-        if dist[node] < weight:
-            continue
-        
-        for next, cost in G[node]:
-            if dist[next]>weight+cost:
-                dist[next] = weight + cost
-                heappush(heap,[dist[next],next])
-    
-    return dist
-    
-INF = int(1e9)
-T= int(input())
-for _ in range(T):
-    # global N,G
-    N,M,K = map(int, input().split())
-    s,g,h = map(int, input().split())
-    
-    G=[[]for _ in range(N+1)]
-    
-    for _ in range(M):
-        u,v,w = map(int, input().split())
-        G[u].append([v,w])
-        G[v].append([u,w])
-        
-    candidate = {}
-    for _ in range(K):
-        candidate[int(input())]=1
+        current_dist, v = heappop(heap)
 
-    distFromS = deijkstra(s)
-    if distFromS[g] > distFromS[h]:
-        newStart = g
-    elif distFromS[g] < distFromS[h]:
-        newStart = h
+        if distance[v] < current_dist: continue
+
+        for w, dist in G[v].items():
+            next_dist = current_dist + dist
+            if distance[w] > next_dist:
+                distance[w] = next_dist
+                heappush(heap, (next_dist, w))
+
+    return distance
+
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+T = int(input())
+for _ in range(T):
+    n, m ,t = map(int, input().split())             # n: 교차로(노드), m: 도로(간선), t: 목적지 후보 개수수
+    s, g, h = map(int, input().split())             # s: 출발지, g, h: 중간에 거쳐간 교차로
+    G = [{} for _ in range(n + 1)]                  # 간선 정보, G[v][w] : v -> w로 가는 비용
+    for _ in range(m):
+        a, b, d = map(int, input().split())         # 교차로 a, b, 비용 d
+        G[a][b] = d
+        G[b][a] = d
+
+    end_list = [int(input()) for _ in range(t)]      # 목적지 후보(t개), 모두 s와 같지 않음
+
+    distFrom_s = Dijkstra(n, s)
+    if distFrom_s[g] > distFrom_s[h]:
+        newS = g 
+    elif distFrom_s[g] < distFrom_s[h]:
+        newS = h
         
-    distFromNewS = deijkstra(newStart)
-    answer = []
-    for i in candidate:
-        if distFromS[newStart] + distFromNewS[i] == distFromS[i]:
-            answer.append(i)
-    answer.sort()
-    print(*answer)
+    distFrom_newS = Dijkstra(n, newS)
+    result = []
+    for end in end_list:
+        if distFrom_s[newS] + distFrom_newS[end] == distFrom_s[end]:
+            result.append(end)
+    print(*sorted(result))
