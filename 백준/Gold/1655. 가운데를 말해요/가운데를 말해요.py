e8e9b4687a72a06f9c922704c5636f5ca0a5a9d7
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-01-22 23:40:09 +0900

    [Gold II] Title: 가운데를 말해요, Time: 176 ms, Memory: 45332 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -4,31 +4,27 @@ from heapq import *
 input = sys.stdin.readline
 N = int(input())
 
-l_heap = [-int(input())]      # 중간 값과 같거나 작은 수, 최대힙
-r_heap = []             # 중간 값보다 큰 수, 최소힙
+left = [-int(input())]      # 중간 값과 같거나 작은 수, 최대힙
+right = []                  # 중간 값보다 큰 수, 최소힙
 
-balance = 0    # l_heap이 많으면 0, 같으면 1
+ans = [str(-left[0])]       # 결과 값 (한번에 출력하기 위한 배열)
+count = 1                   # 입력된 수의 갯수
 
-print(-l_heap[0])
 if N > 1:
     for i in range(N-1):
         num = int(input())
-        if -l_heap[0] >= num:   # 중간 값보다 같거나 작은 숫자인 경우,
 
-            heappush(l_heap, -num)
-            if not balance:
-                heappush(r_heap, -heappop(l_heap))
-                balance = 1
-            else:
-                balance = 0
-            print(-l_heap[0])
+        if -left[0] >= num:         # 입력된 수가 중간 값보다 같거나 작은 경우
+            heappush(left, -num)    # 일단 작은 쪽(left) 배열에 넣고
+            if count % 2 == 1:      # 입력된 수가 홀수인 경우 (len(left) > len(right))
+                heappush(right, -heappop(left))     # left 배열의 가장 큰 수를 right 배열로 이동
 
-        elif -l_heap[0] < num:    # 중간 값보다 더 큰 숫자인 경우
+        elif -left[0] < num:        # 입력된 수가 중간 값보다 더 큰 경우
 
-            heappush(r_heap, num)
-            if balance:
-                heappush(l_heap, -heappop(r_heap))
-                balance = 0
-            else:
-                balance = 1
-            print(-l_heap[0])
+            heappush(right, num)
+            if count % 2 == 0:
+                heappush(left, -heappop(right))
+
+        ans.append(str(-left[0]))
+        count += 1
+print('\n'.join(ans))
