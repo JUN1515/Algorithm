4027af363d5ea00ba259cca08097cbf7be262634
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-01-31 10:08:51 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 1024 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.py
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.py
@@ -2,24 +2,21 @@ import sys
 input = sys.stdin.readline
 
 def find_parent(x):
-    if parent[x] < 0:
-        return x
-    parent[x] = find_parent(parent[x])
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
     return parent[x]
 
 
 def union_parent(x, y):
-    if parent[x] > parent[y]:
+    if x < y:
         parent[x] = y
     else:
-        if parent[x] == parent[y]:
-            parent[x] -= 1
         parent[y] = x
 
 
 for tc in range(1, int(input()) + 1):
     n = int(input())    # 유저의 수
-    parent = [-1] * n
+    parent = [i for i in range(n)]
 
     k = int(input())    # 친구 관계의 수
     for _ in range(k):
