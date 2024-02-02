951ddead30f72c2e747032a67046a059a2a8dc90
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-02-03 00:25:13 +0900

    [Gold IV] Title: 트리, Time: 200 ms, Memory: 37072 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -1,59 +1,55 @@
 import sys
 input = sys.stdin.readline
 
+def dfs(G, visited, start):  # G: 인접행렬, visited: 방문 배열, start: 시작위치
+    S = [start]
+    visited[start] = 1
+    v = start
 
-def find_parent(parent, x):
-    if parent[x] < 0:
-        return x
-    parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+    is_tree = True
+    while S:
+        for w in G[v]:
+            if not visited[w]:    # 방문한 적이 없다면
+                S.append(v)
+                visited[w] = 1
+                v = w
+                break
 
+            if visited[w] == 1 and w != S[-1]:     # 방문한 적 있고, 그 이전 경로가 아니라면
+                is_tree = False                     # 싸이클 생성된것 -> 트리가 아님
 
-def union_parent(parent, x, y):
-    if parent[x] > parent[y]:
-        parent[x] = y
-    else:
-        if parent[x] == parent[y]:
-            parent[x] -= 1
-        parent[y] = x
+        else:
+            # 끝 부분에 도달한 경우, 이전 경로로 돌아가야 함
+            # 하지만 싸이클 생성과 구분하기 위해 추가로 1을 더 더해줌
+            # 즉 2는 이미 갔던 가지에 해당(싸이클은 아님)
+            visited[v] += 1
+            v = S.pop()
+    return is_tree
 
 
 def solution(n, m):
-    parent = [-1] * (n + 1)
-    arr = [list(map(int, input().split())) for _ in range(m)]
 
-    # 서로소 알고리즘
-    for i in range(m):
-        a, b = arr[i]
-        x, y = find_parent(parent, a), find_parent(parent, b)
-        if x != y:
-            union_parent(parent, x, y)
+    G = [[] for _ in range(n + 1)]  # 인접리스트
+    for _ in range(m):
+        u, v = map(int, input().split())
+        G[u].append(v)
+        G[v].append(u)
 
-    r_nodes = {} # key: 루트 노드, value: [집합에 속한 노드 수, 간선 수]
+    visited = [0] * (n + 1)
 
-    # 집합의 개수 확인(루트 노드의 개수로 확인)
+    tree_count = 0
     for i in range(1, n + 1):
-        key = find_parent(parent, i)
-        r_nodes[key] = r_nodes.get(key, 0) + 1
+        if visited[i]: continue     # 이미 집합에 포함되어 있는 경우
+        if dfs(G, visited, i):
+            tree_count += 1
 
-    # 집합의 간선 수 확인(루트 노드로 집합 구분)
-    for i in range(m):
-        key = find_parent(parent, arr[i][0])
-        r_nodes[key] -= 1
-
-    count = 0
-
-    # 트리를 확인, value값이 1이면 tree
-    for val in r_nodes.values():
-        if val == 1:
-            count += 1
-
-    if count == 0:
+    if tree_count == 0:
         return "No trees."
-    elif count == 1:
+    elif tree_count == 1:
         return "There is one tree."
     else:
-        return  f'A forest of {count} trees.'
+        return  f'A forest of {tree_count} trees.'
+
 
 case = 1
 while True:
