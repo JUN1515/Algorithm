d9e7cd2d0d143d4defb84d45d64029fdc8353c1b
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-02-04 23:47:57 +0900

    [Gold IV] Title: Count Circle Groups, Time: 8912 ms, Memory: 132972 KB -BaekjoonHub

---
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
@@ -0,0 +1,37 @@
+def get_parent(x):
+    if parent[x] == x:
+        return x
+    parent[x] = get_parent(parent[x])
+    return parent[x]
+
+
+def find_group(x1, x2):
+    parent1 = get_parent(x1)
+    parent2 = get_parent(x2)
+    if parent1 > parent2:
+        parent[parent1] = parent2
+    else:
+        parent[parent2] = parent1
+
+
+
+import sys, math
+input = sys.stdin.readline
+tc = int(input())
+for _ in range(tc):
+    n = int(input())
+    array = [list(map(int, input().split())) for _ in range(n)]
+    parent = [i for i in range(n)]
+    for i in range(n):
+        x1, y1, r1 = array[i]
+        for j in range(i+1, n):
+            x2, y2, r2 = array[j]
+            if math.sqrt((x1-x2)**2 + (y1-y2)**2) <= r1+r2:
+                find_group(i, j)
+    count_group = set()
+    for i in range(n):
+        x = get_parent(i)
+        if x not in count_group:
+            count_group.add(x)
+
+    print(len(count_group))
