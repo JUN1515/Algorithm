d38a370427b2e2a1c274a5267950dc2333a97f08
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-02-05 11:27:48 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 552 ms, Memory: 77924 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
@@ -1,43 +1,36 @@
 import sys
 input = sys.stdin.readline
 
-def find_parent(r, c):
-    if parent[r][c] != (r, c):
-        parent[r][c] = find_parent(*parent[r][c])
-    return parent[r][c]
+def delta(r, c, cnt):
+    global arr, visited
 
+    while True:
+        visited[r][c] = cnt
 
-def union_parent(y1, x1, y2, x2):
-    if depth[y1][x1] > depth[y2][x2]:
-        parent[y1][x1] = parent[y2][x2]
-        depth[y1][x1] = 1
-    else:
-        if depth[y1][x1] == depth[y2][x2]:
-            depth[y1][x1] -= 1
-        parent[y2][x2] = parent[y1][x1]
-        depth[y2][x2] = 1
+        dr, dc = direction[arr[r][c]]
+        nr, nc = r + dr, c + dc
+
+        if not visited[nr][nc]:
+            r, c = nr, nc
+
+        else:
+            if visited[r][c] == visited[nr][nc]:
+                return 1
+            return 0
 
 
 N, M = map(int, input().split())    # 행, 열
-parent = [[(j, i) for i in range(M)] for j in range(N)]
-depth = [[-1] * M for _ in range(N)]
 
 arr = [list(input().strip()) for _ in range(N)]
 direction = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
 
-for r in range(N):
-    for c in range(M):
-        r1, c1 = find_parent(*parent[r][c])
+visited = [[0] * M for _ in range(N)]
 
-        dr, dc = direction[arr[r][c]]
-        nr, nc = r + dr, c + dc
-        r2, c2 = find_parent(*parent[nr][nc])
-        if (r1, c1) == (r2, c2): continue
-        union_parent(r1, c1, r2, c2)
-
+num = 1
 result = 0
-for r in range(N):
-    for c in range(M):
-        if depth[r][c] != 1:
-            result += 1
+for row in range(N):
+    for col in range(M):
+        if not visited[row][col]:
+            result += delta(row, col, num)
+            num += 1
 print(result)
