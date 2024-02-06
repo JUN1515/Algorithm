fc5407d2025722efc0f7b710009edf987494057c
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-02-06 19:43:08 +0900

    [Gold V] Title: 수 나누기 게임, Time: 724 ms, Memory: 54992 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -1,23 +1,27 @@
 import sys
-input = sys.stdin.readline
 
-n = int(input())
-arr = list(map(int, input().split()))
-max_num = max(arr) + 1
+def solution():
+    input = sys.stdin.readline
+    n = int(input())
+    arr = list(map(int, input().split()))
+    max_num = max(arr) + 1
 
-has_card = [False] * max_num
-numbers = [0] * max_num
+    has_card = [False] * max_num
+    numbers = [0] * max_num
 
-for i in arr:
-    has_card[i] = True
+    for i in arr:
+        has_card[i] = True
 
-for i in arr:
-    for j in range(2 * i, max_num, i):      # j % i == 0 -> j는 승리
-        if has_card[j]:
-            numbers[j] -= 1
-            numbers[i] += 1
+    for i in arr:
+        for j in range(2 * i, max_num, i):      # j % i == 0 -> j는 승리
+            if has_card[j]:
+                numbers[j] -= 1
+                numbers[i] += 1
 
-result = [0] * n
-for i in range(n):
-    result[i] = numbers[arr[i]]
-print(*result)
+    result = [0] * n
+    for i in range(n):
+        result[i] = numbers[arr[i]]
+    print(*result)
+
+
+solution()
