373bf95fc89c2be849cb9e3ba082ae1c6aebac0b
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-02-08 12:20:50 +0900

    [Gold IV] Title: 스도쿠, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,52 +1,88 @@
-# 행검사
-def row_check(r, num):
-    for x in range(9):
-        if num == sdoku[r][x]:
+def solution():
+
+    # 스도쿠에서 확실히 숫자를 채울 수 있는 부분 사전에 채워넣기
+    def pre_fill():
+        remain_case = True
+        while remain_case:
+            remain_case = False
+
+            for r in range(0, 9, 3):
+                for c in range(0, 9, 3):
+
+                    # 각 3 x 3 부분에서, 행, 열, box를 확인해서 가능한 숫자를 확인
+                    current_box = box_possible[(r // 3) * 3 + (c // 3)]
+                    numbers = [[] for _ in range(10)]
+                    for dr in range(3):
+                        for dc in range(3):
+                            nr, nc = r + dr, c + dc
+                            num = arr[nr][nc]
+                            if num == 0:
+                                possible_nums = row_possible[nr] & col_possible[nc] & current_box
+                                for num in possible_nums:
+                                    numbers[num].append((nr, nc))
+
+                    # 만약 해당 숫자가 가능한 곳이 한 곳 뿐이면 채워넣기
+                    for num in range(1, 10):
+                        if len(numbers[num]) == 1:
+                            nr, nc = numbers[num][0]
+                            arr[nr][nc] = num
+                            row_possible[nr].remove(num)
+                            col_possible[nc].remove(num)
+                            current_box.remove(num)
+                            remain_case = True
+
+    def dfs(cnt, goal):
+        if cnt == goal:
+            return True
+
+        r, c = q[cnt]
+        current_box = box_possible[(r // 3) * 3 + (c // 3)]
+        possible_num = row_possible[r] & col_possible[c] & current_box
+        if not possible_num:
             return False
-    return True
+
+        for num in sorted(possible_num):
+            arr[r][c] = num
+            row_possible[r].remove(num)
+            col_possible[c].remove(num)
+            current_box.remove(num)
+            if dfs(cnt + 1, goal):
+                return True
+            arr[r][c] = 0
+            row_possible[r].add(num)
+            col_possible[c].add(num)
+            current_box.add(num)
+
+    arr = [list(map(int, list(input().strip()))) for _ in range(9)]
+    row = [set() for _ in range(9)]
+    col = [set() for _ in range(9)]
+    box = [set() for _ in range(9)]
+
+    for r in range(9):
+        for c in range(9):
+            num = arr[r][c]
+            if num:
+                row[r].add(num)
+                col[c].add(num)
+                box[(r // 3) * 3 + (c // 3)].add(num)
+
+    full = set(range(1, 10))
+    row_possible = [full - s for s in row]  # 각 행에서 0에 들어갈 수 있는 수
+    col_possible = [full - s for s in col]  # 각 열에서 0에 들어갈 수 있는 수
+    box_possible = [full - s for s in box]  # 각 3x3 에서 0에 들어갈 수 있는 수
+
+    pre_fill()  # 사전에 확실히 채울 수 있는 부분 모두 채우기
+
+    q = []      # 남아있는 빈칸의 좌표
+    for r in range(9):
+        for c in range(9):
+            if arr[r][c] == 0:
+                q.append((r, c))
+
+    dfs(0, len(q))  # dfs로 가능한 경우 찾기
+
+    for lst in arr:
+        print(''.join(map(str, lst)))
 
 
-def col_check(c, num):
-    # 열검사
-    for x in range(9):
-        if num == sdoku[x][c]:
-            return False
-    return True
-
-
-def three_check(r, c, num):
-    # 3x3 검사
-    nc = (c // 3) * 3
-    nr = (r // 3) * 3
-    for x in range(3):
-        for y in range(3):
-            if sdoku[nr + x][nc + y] == num:
-                return False
-    return True
-
-
-def dfs(depth):
-    if depth >= len(zero_p):
-        for k in range(9):
-            print(''.join(map(str, sdoku[k])))
-        exit()
-
-    nr, nc = zero_p[depth]  # 리스트에서 하나씩 튜플로 꺼낸다.
-    for j in range(1, 9 + 1):
-    	#dfs를 돌기전에 검사하여 조건에 맞는 것만 dfs를 돈다
-        if row_check(nr, j) and col_check(nc, j) and three_check(nr, nc, j):
-            sdoku[nr][nc] = j
-            dfs(depth + 1)
-            sdoku[nr][nc] = 0
-
-
-sdoku = []
-zero_p = []  # 2차원 9*9 리스트를 1차원적으로 생각하여 dfs를 돌기 때문에 좌표들을 튜플형식으로 넣는다.
-for i in range(9):
-    temp = list(map(int, input()))
-    for j in range(len(temp)):
-        if temp[j] == 0:
-            zero_p.append((i, j))  # 0의 좌표들을 저장한다.(1차원 리스트에 튜플로 저장)
-    sdoku.append(temp)
-
-dfs(0)
+solution()
