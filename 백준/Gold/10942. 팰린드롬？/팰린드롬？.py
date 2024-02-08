ddc5535dd9bd93678a348f623f47318dd8b6c879
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-02-08 23:44:37 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1284 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -1,34 +1,29 @@
+# dp의 각 요소는 펠린드롬의 시작 위치에 해당.
+# dp[i]의 i는 시작지점과 끝지점의 합을 의미 |(ex i가 4이면, 1 3, 2 2 가 될 수 있음)
+# i = s + e일 때, dp[i] < s 이면 s -> e 는 펠린드롬이다.
+# 만일 i가 4이고, dp[4] = 1 이면, 2 2 는 펠린드롬이지만 1 3 은 펠린드롬이 아님
 import sys
 
 def solution():
     input = sys.stdin.readline
-    N = int(input())                        # 수열의 크기
-    arr = list(map(int, input().split()))
-    dp = [[False] * N for _ in range(N)]    # dp[r][c]는 r부터 c까지 수의 펠린드롬 여부
+    N = int(input())
+    arr = [0] + list(map(int, input().split()))
+    dp = [0] * (N * 2 + 1)
 
-    # Case 1. i -> i 의 경우, 모두 펠린드롬
-    for i in range(N):
-        dp[i][i] = True
+    for i in range(N * 2 + 1):
+        s = e = i // 2
+        e += i & 1  # i가 홀수이면 e에 +1 해주기
 
-    if N > 1:
-        # Case 2. i -> i + 1 의 경우, 양 끝의 두 수가 같아야 펠린드롬
-        for i in range(N - 1):
-            if arr[i] == arr[i + 1]:
-                dp[i][i+1] = True
-
-    if N > 2:
-        # Case 3. i -> i + k (2 <= k < N) 의 경우,
-        # 양 끝의 수(i, i + k)가 같고, 양 끝 안 쪽의 수(i + 1 -> i + k - 1) 들이 펠린드롬이면, 펠린드롬
-        for k in range(2, N):
-            for i in range(N - k):
-                if arr[i] == arr[i + k] and dp[i + 1][i + k - 1]:
-                    dp[i][i + k] = True
+        while s > 0 and e <= N and arr[s] == arr[e]:
+            s -= 1
+            e += 1
+        dp[i] = s   # s + 1 부터 펠린드롬이 가능 
 
     M = int(input())
     result = [0] * M
     for i in range(M):
         S, E = map(int, input().split())
-        if dp[S-1][E-1]:
+        if dp[S + E] < S:
             result[i] = 1
 
     for i in range(M):
