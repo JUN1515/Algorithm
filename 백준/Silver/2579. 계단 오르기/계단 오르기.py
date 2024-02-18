48d12885fa638430dce337f1e2cbfbb4b515bf7a
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-02-18 22:34:48 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N = int(input())
+    arr = [0] + [int(input()) for _ in range(N)]
+    dp = [0] * (N + 1)
+
+    dp[1] = arr[1]
+    if N > 1:
+        dp[2] = arr[1] + arr[2]
+
+    if N > 2:
+        for i in range(3, N + 1):
+            dp[i] = max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i]
+
+    print(dp[N])
+
+
+solution()
