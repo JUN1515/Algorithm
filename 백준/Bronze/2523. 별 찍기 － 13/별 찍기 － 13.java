16bf38f02edf860050cd90fbcc590246df225e0c
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-03-01 14:59:45 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 368 ms, Memory: 18388 KB -BaekjoonHub

--- 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -6,8 +6,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = Integer.parseInt(sc.nextLine());
 
-        for (int i = 1; i <= N * 2 - 1; i++) {
-            for (int j = 1; j <= i - ((i % N) * 2) * (i / (N + 1)); j++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = N - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
