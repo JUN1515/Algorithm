677166c2a5b23583a7fcd37602d912128f51fd82
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-03-21 13:50:43 +0900

    [Silver II] Title: 싸이버개강총회, Time: 132 ms, Memory: 43412 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
@@ -1,31 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-def time_compare(t1, t2):
-    m1 = int(t1[:2]) * 60 + int(t1[3:])
-    m2 = int(t2[:2]) * 60 + int(t2[3:])
-    return True if m1 >= m2 else False
-
 def solution():
 
     S, E, Q = input().strip().split()
-    check = {}
+    check = set()
+    answer = 0
 
     while (log := sys.stdin.readline()):
         time, name = log.strip().split()
 
-        if time_compare(S, time):
-            check[name] = 0
+        if time <= S:
+            check.add(name)
 
-        else:
-            if time_compare(time, E) and time_compare(Q, time):
-                if name in check:
-                    check[name] += 1
+        elif E <= time <= Q:
+            if name in check:
+                check.remove(name)
+                answer += 1
 
-    answer = 0
-    for val in check.values():
-        if val:
-            answer += 1
     print(answer)
 
 
