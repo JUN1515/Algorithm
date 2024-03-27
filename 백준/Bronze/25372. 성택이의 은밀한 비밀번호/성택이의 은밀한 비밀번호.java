d4e384e49774b2d888c9ffd7860b114d85e7fa30
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-03-28 03:00:26 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 144 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            String S = br.readLine();
+            if ((S.length() >= 6) && (S.length() <= 9)) sb.append("yes").append("\n");
+            else sb.append("no").append("\n");
+            
+            
+        }
+        System.out.println(sb);
+
+    }
+}
