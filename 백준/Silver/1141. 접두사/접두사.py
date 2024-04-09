d71ad4d462f3c6864a031081548c7c81f8806435
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-04-09 19:48:21 +0900

    [Silver I] Title: 접두사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N = int(input().strip())
+    arr = [input().strip() for _ in range(N)]
+    arr.sort(reverse=True)
+    answer = N
+    for i in range(1, N):
+        for j in range(i - 1, -1, -1):
+            if arr[i][0] != arr[j][0]:
+                break
+
+            if len(arr[i]) > len(arr[j]):
+                continue
+
+            if arr[i] == arr[j][:len(arr[i])]:
+                answer -= 1
+                break
+    print(answer)
+    
+
+solution()
