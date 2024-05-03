3495f1f693a1e2b419863ed271b4900c464233d2
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-05-03 21:12:20 +0900

    [Bronze V] Title: 웰컴, Time: 120 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n")
+            .append("|  | _ | _. _ ._ _  _\n")
+            .append("|/\\|(/.|(_.(_)[ | )(/.");
+        
+        System.out.println(sb);
+    }
+}
