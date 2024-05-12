caa42b55d5313639c227185febb25a45ec6d25d4
JUN1515 <49270012+JUN1515@users.noreply.github.com>
2024-05-13 00:26:25 +0900

    [Bronze V] Title: 연세대학교, Time: 208 ms, Memory: 17400 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        if (N == 1) {
+            System.out.println("Leading the Way to the Future");
+        } else {
+            System.out.println("YONSEI");
+        }
+    }
+}
